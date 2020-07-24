98be0afc3f31fe787cc503ca7eb2d6a8dc75619c
n-miyao <68102508+n-miyao@users.noreply.github.com>
2020-07-24 19:49:31 +0900

    Initial commit.
    練習

---
+++ index.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+
+async function getUsers(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    return users;
+}
+
+async function listUsers(){
+    // データのやりとり
+    const users = await getUsers();
+   
+    // DOM操作
+    users.forEach(function(user) {
+        const list = document.createElement("li");
+        list.innerText = user.name;
+        lists.appendChild(list);
+    });
+}
+
+button.addEventListener("click", listUsers);
+
+window.addEventListener("load", listUsers);
